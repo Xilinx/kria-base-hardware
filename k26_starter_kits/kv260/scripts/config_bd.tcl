ee7532997e731d4c8f87eccd4af4466b89b8ef47
sweatha rao <sweathar@xilinx.com>
2022-01-28 13:59:33 -0800

    k26_starter_kits: Added scripts to generate xsa for base, kv260 and kr260

---
+++ k26_starter_kits/kv260/scripts/config_bd.tcl
@@ -0,0 +1,48 @@
+
+
+proc create_kv260_starter_kit { parentCell nameHier } {
+
+set parentObj [get_bd_cells $parentCell]
+
+set oldCurInst [current_bd_instance .]
+
+current_bd_instance $parentObj
+
+if {$nameHier ne "" } {
+
+set hier_obj [create_bd_cell -type hier $nameHier]
+
+current_bd_instance $hier_obj
+
+}
+set ::PS_INST zynq_ultra_ps_e_0
+set zynq_ultra_ps_e_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:zynq_ultra_ps_e zynq_ultra_ps_e_0 ] 
+
+ apply_bd_automation -rule xilinx.com:bd_rule:zynq_ultra_ps_e -config {apply_board_preset "1"} [get_bd_cells zynq_ultra_ps_e_0] 
+
+set_property -dict [ list \
+CONFIG.PSU__CRF_APB__DPLL_FRAC_CFG__ENABLED  {1} \
+CONFIG.PSU__CRF_APB__VPLL_FRAC_CFG__ENABLED  {1} \
+CONFIG.PSU__CRL_APB__USB3__ENABLE {1} \
+CONFIG.PSU__FPGA_PL0_ENABLE {1} \
+CONFIG.PSU__NUM_FABRIC_RESETS {4} \
+CONFIG.PSU__USE__M_AXI_GP0  {0} \
+CONFIG.PSU__USE__M_AXI_GP1  {0} \
+CONFIG.PSU__USE__M_AXI_GP2  {0} \
+CONFIG.PSU__USE__S_AXI_ACE  {0} \
+CONFIG.PSU__USE__S_AXI_ACP  {0} \
+CONFIG.PSU__USE__S_AXI_GP0  {0} \
+CONFIG.PSU__USE__S_AXI_GP1  {0} \
+CONFIG.PSU__USE__S_AXI_GP2  {0} \
+CONFIG.PSU__USE__S_AXI_GP3  {0} \
+CONFIG.PSU__USE__S_AXI_GP4  {0} \
+CONFIG.PSU__USE__S_AXI_GP5  {0} \
+CONFIG.PSU__USE__S_AXI_GP6  {0} \
+] [get_bd_cells zynq_ultra_ps_e_0] 
+
+
+current_bd_instance $oldCurInst
+
+}
+create_kv260_starter_kit "" ""
+
